b654a152e3ebd2837b78ae9bd48f662ece58a656
WenceyWang <henzichao@gmail.com>
2014-06-09 22:33:36 +0800

    update

--- OrangeNoEnd/OrangeNoEnd.Windows/MainPage.xaml.cs
+++ OrangeNoEnd/OrangeNoEnd.Windows/MainPage.xaml.cs
@@ -20,357 +20,410 @@ namespace OrangeNoEnd
 {
 
 
-    /// <summary>
-    /// 可独立使用或用于导航至 Frame 内部的空白页。
-    /// </summary>
-    /// 
-    public sealed partial class MainPage : Page
-    {
-        ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+	/// <summary>
+	/// 可独立使用或用于导航至 Frame 内部的空白页。
+	/// </summary>
+	/// 
+	public sealed partial class MainPage : Page
+	{
+		ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
 
-        #region Settings
-        List<int> SettingOfCursor = new List<int>();
-        List<int> SettingOfPrimary = new List<int>();
-        List<int> SettingOfFarm = new List<int>();
-        List<int> SettingOfMine = new List<int>();
-        List<int> SettingOfLaboratory = new List<int>();
-        List<int> SettingOfSpaceCraft = new List<int>();
-        List<int> SettingOfReactor = new List<int>();
-        List<int> SettingOfML = new List<int>();
-        #endregion
+		#region Settings
+		List<int> SettingOfRush = new List<int>();
+		List<int> SettingOfCursor = new List<int>();
+		List<int> SettingOfPrimary = new List<int>();
+		List<int> SettingOfFarm = new List<int>();
+		List<int> SettingOfMine = new List<int>();
+		List<int> SettingOfLaboratory = new List<int>();
+		List<int> SettingOfSpaceCraft = new List<int>();
+		List<int> SettingOfReactor = new List<int>();
+		List<int> SettingOfML = new List<int>();
+		#endregion
 
-        #region Timers
-        DispatcherTimer TimerForCursor = new DispatcherTimer();
-        DispatcherTimer TimerForPrimary = new DispatcherTimer();
-        DispatcherTimer TimerForFarm = new DispatcherTimer();
-        DispatcherTimer TimerForMine = new DispatcherTimer();
-        DispatcherTimer TimerForLaboratory = new DispatcherTimer();
-        DispatcherTimer TimerForSpaceCraft = new DispatcherTimer();
-        DispatcherTimer TimerForReactor = new DispatcherTimer();
-        DispatcherTimer TimerForML = new DispatcherTimer();
+		#region Timers
+		DispatcherTimer TimerForCursor = new DispatcherTimer();
+		DispatcherTimer TimerForPrimary = new DispatcherTimer();
+		DispatcherTimer TimerForFarm = new DispatcherTimer();
+		DispatcherTimer TimerForMine = new DispatcherTimer();
+		DispatcherTimer TimerForLaboratory = new DispatcherTimer();
+		DispatcherTimer TimerForSpaceCraft = new DispatcherTimer();
+		DispatcherTimer TimerForReactor = new DispatcherTimer();
+		DispatcherTimer TimerForML = new DispatcherTimer();
 
-        DispatcherTimer TimerForCleanEvent = new DispatcherTimer();
-        #endregion
+		DispatcherTimer TimerForCleanEvent = new DispatcherTimer();
+		#endregion
 
-        public MainPage()
-        {
-            this.InitializeComponent();
+		public MainPage()
+		{
+			this.InitializeComponent();
 
-            #region SetStartValue
-            //if (localSettings.Values["GameIsStart"]==null)
-            {
-                SetStartValue();
-                localSettings.Values["GameIsStart"] = true;
-            }
-            #endregion
+			#region SetStartValue
+			SettingOfRush.Add(1);
+			SettingOfPrimary.Add(1);
+			SettingOfFarm.Add(1);
+			//if (localSettings.Values["GameIsStart"]==null)
+			{
+				SetStartValue();
+				localSettings.Values["GameIsStart"] = true;
+			}
+			#endregion
 
-            #region SetTimer
-            TimerForCleanEvent.Interval = new TimeSpan(0, 0, 1);
-            TimerForCleanEvent.Tick += TimerForCleanEvent_Tick;
-            TimerForCursor.Interval = new TimeSpan(0, 0, 1);
-            TimerForCursor.Tick += TimerForCursor_Tick;
-            TimerForPrimary.Interval = new TimeSpan(0, 0, 1);
-            TimerForPrimary.Tick += TimerForPrimy_Tick;
+			#region SetTimer
+			TimerForCleanEvent.Interval = new TimeSpan(0, 0, 1);
+			TimerForCleanEvent.Tick += TimerForCleanEvent_Tick;
+			TimerForCursor.Interval = new TimeSpan(0, 0, 1);
+			TimerForCursor.Tick += TimerForCursor_Tick;
+			TimerForPrimary.Interval = new TimeSpan(0, 0, 1);
+			TimerForPrimary.Tick += TimerForPrimy_Tick;
 
-            #endregion
+			#endregion
 
-            #region StartTimer
-            TimerForPrimary.Start();
-            TimerForCursor.Start();
-            #endregion
+			#region StartTimer
+			TimerForPrimary.Start();
+			TimerForCursor.Start();
+			#endregion
 
-        }
-        void SetStartValue()
-        {
-            NumberOfOrange = NumberOfCursor = NumberOfPrimary = NumberOfFarm = NumberOfMine = NumberOfLaboratory = NumberOfSpaceCraft = NumberOfReactor = NumberOfML = 0;
-            LevelOfRush = LevelOfCursor = LevelOfPrimary = LevelOfFarm = LevelOfMine = LevelOfLaboratory = LevelOfSpaceCraft = LevelOfReactor = LevelOfML = 1;
-        }
+		}
+		void SetStartValue()
+		{
+			NumberOfOrange =100000000000000000m;//= 
+			NumberOfCursor = NumberOfPrimary = NumberOfFarm = NumberOfMine = NumberOfLaboratory = NumberOfSpaceCraft = NumberOfReactor = NumberOfML = 100;
+			LevelOfRush = LevelOfCursor = LevelOfPrimary = LevelOfFarm = LevelOfMine = LevelOfLaboratory = LevelOfSpaceCraft = LevelOfReactor = LevelOfML =0;
+		}
 
-        decimal NumberOfOrange
-        {
-            get
-            {
-                return Convert.ToDecimal(localSettings.Values["NumberOfOrange"]);
-            }
-            set
-            {
-                NumberOfOrangeOut.Text = value.ToString();
-                localSettings.Values["NumberOfOrange"] = value.ToString();
-            }
-        }
+		#region Of
 
-        int NumberOfCursor
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["NumberOfCursor"]);
-            }
-            set
-            {
-                NumberOfCursorOut.Text = value.ToString();
-                localSettings.Values["NumberOfCursor"] = value;
-            }
-        }
+		#region NumberOf
+		decimal NumberOfOrange
+		{
+			get
+			{
+				return Convert.ToDecimal(localSettings.Values["NumberOfOrange"]);
+			}
+			set
+			{
+				NumberOfOrangeOut.Text = value.ToString();
+				localSettings.Values["NumberOfOrange"] = value.ToString();
+			}
+		}
+		int NumberOfCursor
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["NumberOfCursor"]);
+			}
+			set
+			{
+				NumberOfCursorOut.Text = value.ToString();
+				localSettings.Values["NumberOfCursor"] = value;
+			}
+		}
+		int NumberOfPrimary
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["NumberOfPrimary"]);
+			}
+			set
+			{
+				NumberOfPrimaryOut.Text = value.ToString();
+				localSettings.Values["NumberOfPrimary"] = value;
+			}
+		}
+		int NumberOfFarm
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["NumberOfFarm"]);
+			}
+			set
+			{
+				NumberOfFarmOut.Text = value.ToString();
+				localSettings.Values["NumberOfFarm"] = value;
+			}
+		}
+		int NumberOfMine
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["NumberOfMine"]);
+			}
+			set
+			{
+				NumberOfMineOut.Text = value.ToString();
+				localSettings.Values["NumberOfMine"] = value;
+			}
+		}
+		int NumberOfLaboratory
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["NumberOfLaboratory"]);
+			}
+			set
+			{
+				NumberOfLaboratoryOut.Text = value.ToString();
+				localSettings.Values["NumberOfLaboratory"] = value;
+			}
+		}
+		int NumberOfSpaceCraft
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["NumberOfSpaceCraft"]);
+			}
+			set
+			{
+				NumberOfSpaceCraftOut.Text = value.ToString();
+				localSettings.Values["NumberOfSpaceCraft"] = value;
+			}
+		}
+		int NumberOfReactor
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["NumberOfReactor"]);
+			}
+			set
+			{
+				NumberOfReactorOut.Text = value.ToString();
+				localSettings.Values["NumberOfReactor"] = value;
+			}
+		}
+		int NumberOfML
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["NumberOfML"]);
+			}
+			set
+			{
+				NumberOfMLOut.Text = value.ToString();
+				localSettings.Values["NumberOfML"] = value;
+			}
+		}
+		#endregion
 
-        int NumberOfPrimary
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["NumberOfPrimary"]);
-            }
-            set
-            {
-                NumberOfPrimaryOut.Text = value.ToString();
-                localSettings.Values["NumberOfPrimary"] = value;
-            }
-        }
+		#region LevelOf
+		int LevelOfRush
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["LevelOfRush"]);
+			}
+			set
+			{
+				localSettings.Values["LevelOfRush"] = value;
+			}
+		}
+		int LevelOfCursor
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["LevelOfCursor"]);
+			}
+			set
+			{
+				LevelOfCursorOut.Text = value.ToString();
+				localSettings.Values["LevelOfCursor"] = value;
+			}
+		}
+		int LevelOfPrimary
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["LevelOfPrimary"]);
+			}
+			set
+			{
+				LevelOfPrimaryOut.Text = value.ToString();
+				localSettings.Values["LevelOfPrimary"] = value;
+			}
+		}
+		int LevelOfFarm
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["LevelOfFarm"]);
+			}
+			set
+			{
+				LevelOfFarmOut.Text = value.ToString();
+				localSettings.Values["LevelOfFarm"] = value;
+			}
+		}
+		int LevelOfMine
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["LevelOfMine"]);
+			}
+			set
+			{
+				LevelOfMineOut.Text = value.ToString();
+				localSettings.Values["LevelOfMine"] = value;
+			}
+		}
+		int LevelOfLaboratory
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["LevelOfLaboratory"]);
+			}
+			set
+			{
+				LevelOfLaboratoryOut.Text = value.ToString();
+				localSettings.Values["LevelOfLaboratory"] = value;
+			}
+		}
+		int LevelOfSpaceCraft
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["LevelOfSpaceCraft"]);
+			}
+			set
+			{
+				LevelOfSpaceCraftOut.Text = value.ToString();
+				localSettings.Values["LevelOfSpaceCraft"] = value;
+			}
+		}
+		int LevelOfReactor
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["LevelOfReactor"]);
+			}
+			set
+			{
+				LevelOfReactorOut.Text = value.ToString();
+				localSettings.Values["LevelOfReactor"] = value;
+			}
+		}
+		int LevelOfML
+		{
+			get
+			{
+				return Convert.ToInt32(localSettings.Values["LevelOfML"]);
+			}
+			set
+			{
+				LevelOfMLOut.Text = value.ToString();
+				localSettings.Values["LevelOfML"] = value;
+			}
+		}
+		#endregion
 
-        int NumberOfFarm
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["NumberOfFarm"]);
-            }
-            set
-            {
-                NumberOfFarmOut.Text = value.ToString();
-                localSettings.Values["NumberOfFarm"] = value;
-            }
-        }
+		#region RiseOf
+		int RiseOfRush
+		{
+			get
+			{
+				return SettingOfRush[LevelOfRush];
+			}
+		}
+		int RiseOfCursor
+		{
+			get
+			{
+				return SettingOfCursor[LevelOfCursor];
+			}
+		}
+		int RiseOfPrimary
+		{
+			get
+			{
+				return SettingOfPrimary[LevelOfPrimary];
+			}
+		}
+		int RiseOfFarm
+		{
+			get
+			{
+				return SettingOfFarm[LevelOfFarm];
+			}
+		}
+		int RiseOfMine
+		{
+			get
+			{
+				return SettingOfMine[LevelOfMine];
+			}
+		}
+		int RiseOfLaboratory
+		{
+			get
+			{
+				return SettingOfLaboratory[LevelOfLaboratory];
+			}
+		}
+		int RiseOfSpaceCraft
+		{
+			get
+			{
+				return SettingOfSpaceCraft[LevelOfSpaceCraft];
+			}
+		}
+		int RiseOfReactor
+		{
+			get
+			{
+				return SettingOfReactor[LevelOfReactor];
+			}
+		}
+		int RiseOfML
+		{
+			get
+			{
+				return SettingOfML[LevelOfML];
+			}
+		}
+		#endregion
 
-        int NumberOfMine
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["NumberOfMine"]);
-            }
-            set
-            {
-                NumberOfMineOut.Text = value.ToString();
-                localSettings.Values["NumberOfMine"] = value;
-            }
-        }
+		#endregion
 
-        int NumberOfLaboratory
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["NumberOfLaboratory"]);
-            }
-            set
-            {
-                NumberOfLaboratoryOut.Text = value.ToString();
-                localSettings.Values["NumberOfLaboratory"] = value;
-            }
-        }
+		public void ShowEvent(string text)
+		{
+			TBEvent.Text = text;
+			TimerForCleanEvent.Start();
+		}
 
-        int NumberOfSpaceCraft
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["NumberOfSpaceCraft"]);
-            }
-            set
-            {
-                NumberOfSpaceCraftOut.Text = value.ToString();
-                localSettings.Values["NumberOfSpaceCraft"] = value;
-            }
-        }
+		void TimerForCursor_Tick(object sender, object e)
+		{
+			NumberOfOrange += LevelOfCursor;
+			ShowEvent(localSettings.Values["LevelOfCursor"].ToString() + "个橘子从树上掉了下来~~~");
+		}
 
-        int NumberOfReactor
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["NumberOfReactor"]);
-            }
-            set
-            {
-                NumberOfReactorOut.Text = value.ToString();
-                localSettings.Values["NumberOfReactor"] = value;
-            }
-        }
+		void TimerForCleanEvent_Tick(object sender, object e)
+		{
+			TBEvent.Text = "";
+			TimerForCleanEvent.Stop();
+		}
 
-        int NumberOfML
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["NumberOfML"]);
-            }
-            set
-            {
-                NumberOfMLOut.Text = value.ToString();
-                localSettings.Values["NumberOfML"] = value;
-            }
-        }
-        int LevelOfRush
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["LevelOfRush"]);
-            }
-            set
-            {
-                LevelOfRushOut.Text = value.ToString();
-                localSettings.Values["LevelOfRush"] = value;
-            }
-        }
+		void TimerForPrimy_Tick(object sender, object e)
+		{
+			NumberOfOrange +=RiseOfPrimary*NumberOfPrimary;
+		}
 
-        int LevelOfCursor
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["LevelOfCursor"]);
-            }
-            set
-            {
-                LevelOfCursorOut.Text = value.ToString();
-                localSettings.Values["LevelOfCursor"] = value;
-            }
-        }
+		private void NumberOfOrangeOut_Loaded(object sender, RoutedEventArgs e)
+		{
 
-        int LevelOfPrimary
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["LevelOfPrimary"]);
-            }
-            set
-            {
-                LevelOfPrimaryOut.Text = value.ToString();
-                localSettings.Values["LevelOfPrimary"] = value;
-            }
-        }
+		}
 
-        int LevelOfFarm
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["LevelOfFarm"]);
-            }
-            set
-            {
-                LevelOfFarmOut.Text = value.ToString();
-                localSettings.Values["LevelOfFarm"] = value;
-            }
-        }
+		private void BRush_PointerEntered(object sender, PointerRoutedEventArgs e)
+		{
+			TBInfo.Text = "摇晃橘子树~~橘子可能会从树上掉下来，每次摇树会掉下" + RiseOfRush + "个橘子";
+		}
 
-        int LevelOfMine
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["LevelOfMine"]);
-            }
-            set
-            {
-                LevelOfMineOut.Text = value.ToString();
-                localSettings.Values["LevelOfMine"] = value;
-            }
-        }
+		private void _PointerExited(object sender, PointerRoutedEventArgs e)
+		{
+			TBInfo.Text = "";
+		}
 
-        int LevelOfLaboratory
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["LevelOfLaboratory"]);
-            }
-            set
-            {
-                LevelOfLaboratoryOut.Text = value.ToString();
-                localSettings.Values["LevelOfLaboratory"] = value;
-            }
-        }
-
-        int LevelOfSpaceCraft
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["LevelOfSpaceCraft"]);
-            }
-            set
-            {
-                LevelOfSpaceCraftOut.Text = value.ToString();
-                localSettings.Values["LevelOfSpaceCraft"] = value;
-            }
-        }
-
-        int LevelOfReactor
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["LevelOfReactor"]);
-            }
-            set
-            {
-                LevelOfReactorOut.Text = value.ToString();
-                localSettings.Values["LevelOfReactor"] = value;
-            }
-        }
-
-        int LevelOfML
-        {
-            get
-            {
-                return Convert.ToInt32(localSettings.Values["LevelOfML"]);
-            }
-            set
-            {
-                LevelOfMLOut.Text = value.ToString();
-                localSettings.Values["LevelOfML"] = value;
-            }
-        }
-
-        public void ShowEvent(string text)
-        {
-            TBEvent.Text = text;
-            TimerForCleanEvent.Start();
-        }
-
-        public void UpdateTheNumberOfOranges()
-        {
-            NumberOfOrangeOut.Text = (string)localSettings.Values["Oranges"];
-        }
-
-
-
-        void TimerForCursor_Tick(object sender, object e)
-        {
-            NumberOfOrange += LevelOfCursor;
-            ShowEvent(localSettings.Values["LevelOfCursor"].ToString() + "个橘子从树上掉了下来~~~");
-        }
-
-        void TimerForCleanEvent_Tick(object sender, object e)
-        {
-            TBEvent.Text = "";
-            TimerForCleanEvent.Stop();
-        }
-
-        void TimerForPrimy_Tick(object sender, object e)
-        {
-
-        }
-
-        private void NumberOfOrangeOut_Loaded(object sender, RoutedEventArgs e)
-        {
-            UpdateTheNumberOfOranges();
-        }
-
-
-        private void BRush_PointerEntered(object sender, PointerRoutedEventArgs e)
-        {
-            TBInfo.Text = "摇晃橘子树~~橘子可能会从树上掉下来，每次摇树会掉下" + localSettings.Values["LevelOfRush"].ToString() + "个橘子";
-        }
-
-        private void _PointerExited(object sender, PointerRoutedEventArgs e)
-        {
-            TBInfo.Text = "";
-        }
-
-        private void BRush_Click(object sender, RoutedEventArgs e)
-        {
-            NumberOfOrange++;
-            ShowEvent(localSettings.Values["LevelOfRush"].ToString() + "个橘子从树上掉了下来~~~");
-        }
-
-
-    }
+		private void BRush_Click(object sender, RoutedEventArgs e)
+		{
+			NumberOfOrange+=RiseOfRush;
+			ShowEvent(RiseOfRush.ToString() + "个橘子从树上掉了下来~~~");
+		}
+	}
 }
